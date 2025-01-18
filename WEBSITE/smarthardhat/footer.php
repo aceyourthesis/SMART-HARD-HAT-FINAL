c154fb99b54ffc6204248469ff22dd98d9d4643a
j4javier <jfourjavier@gmail.com>
2025-01-18 23:40:23 +0800

    ADD FINAL WEBSITE

---
+++ WEBSITE/smarthardhat/footer.php
@@ -0,0 +1,7 @@
+        <section id="footer">
+            <div class="container footer text-center">
+                <small>&copy; 2025Cavite State University - Main Campus</small>
+            </div>
+        </section>
+    </body>
+</html>
